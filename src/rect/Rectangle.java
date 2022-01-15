dc73889d33b51fcdad5abacaba58491882eb7f35
Werapat Wangrungroj <hellp002@gmail.com>
2022-01-15 15:17:37 +0700

    Add Rectangle class

---
+++ src/rect/Rectangle.java
@@ -0,0 +1,22 @@
+package rect;
+
+public class Rectangle {
+	private int width;
+	private int height;
+	
+	public Rectangle(int width, int height) {
+		super();
+		this.width = Math.max(1, width);
+		this.height = Math.max(1, height);
+	}
+
+	public int getWidth() {
+		return width;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+	
+	
+}
